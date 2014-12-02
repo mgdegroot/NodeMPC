70ad44e1b6fffb66ce0262c280e82aaa3465cb8b
Marcel <mg.degroot@openmailbox.org>
2014-12-02 08:59:11 +0100

    Tests

--- test/mpc_commandsTest.js
+++ test/mpc_commandsTest.js
@@ -109,6 +109,116 @@ describe("mpc_commands", function() {
             assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
         });
 
+        it ("should send setvol 60 command", function() {
+            var expectedWrite = "setvol 60\n";
+            mpc_commands.setVolume(60);
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send setvol 55 command", function() {
+            var expectedWrite = "setvol 55\n";
+            mpc_commands.evalExpr("currentAudioVolume = 50");
+            mpc_commands.volumeInc();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send setvol 45 command", function() {
+            var expectedWrite = "setvol 45\n";
+            mpc_commands.evalExpr("currentAudioVolume = 50");
+            mpc_commands.volumeDec();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send status command", function() {
+            var expectedWrite = "status\n";
+            mpc_commands.getStatus();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send stats command", function() {
+            var expectedWrite = "stats\n";
+            mpc_commands.getStats();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send playlistinfo command", function() {
+            var expectedWrite = "playlistinfo\n";
+            mpc_commands.getCurrentPlaylist();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send close command", function() {
+            var expectedWrite = "close\n";
+            mpc_commands.disconnect();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send listdir command", function() {
+            var expectedWrite = "listall\n";
+            mpc_commands.listdir();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send listdir directory command", function() {
+            var expectedWrite = "listall directory\n";
+            mpc_commands.listdir("directory");
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send update command", function() {
+            var expectedWrite = "update\n";
+            mpc_commands.update();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send update directory command", function() {
+            var expectedWrite = "update directory\n";
+            mpc_commands.update("directory");
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send listplaylists command", function() {
+            var expectedWrite = "listplaylists\n";
+            mpc_commands.list();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send clear and load command", function() {
+            var expectedWrite = "clear\n";
+            mpc_commands.loadPlaylist("test");
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+            mpc_commands.evalExpr("internalCommandCallback = function(){};");
+            mpc_commands.loadPlaylist();
+            expectedWrite = "load test\n";
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send clear command", function() {
+            var expectedWrite = "clear\n";
+            mpc_commands.clearPlaylist();
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send save command", function() {
+            var expectedWrite = "save playlistname\n";
+            mpc_commands.savePlaylist("playlistname");
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should send search command", function() {
+            var expectedWrite = "search any searchterm\n";
+            mpc_commands.searchDb("searchterm");
+            assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
+        it ("should test evel", function() {
+            //var expectedWrite = "update\n";
+            mpc_commands.evalExpr("currentAudioVolume = 80");
+            var res = mpc_commands.evalExpr("currentAudioVolume");
+            console.log(res);
+            //assert.equal(fakeConnection.lastDataInWrite, expectedWrite);
+        });
+
     });
 
 
